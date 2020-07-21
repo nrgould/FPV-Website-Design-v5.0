e3302f918999880e94a1ce1a22f476f7dc42fa6f
Nicholas Gould <nrgould197@gmail.com>
2020-07-20 20:23:15 -0400

    update hero section

---
+++ script.js
@@ -0,0 +1,23 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(
+	/\S/g,
+	"<span class='letter'>$&</span>"
+);
+
+anime
+	.timeline({ loop: false })
+	.add({
+		targets: '.ml3 .letter',
+		opacity: [0, 1],
+		easing: 'easeInOutQuad',
+		duration: 250,
+		delay: (el, i) => 50 * (i + 1),
+	})
+	.add({
+		targets: '.ml3',
+		opacity: 1,
+		duration: 250,
+		easing: 'easeOutExpo',
+		delay: 0,
+	});
