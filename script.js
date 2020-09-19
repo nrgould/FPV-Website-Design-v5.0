8e12e8ea2b8c3e60ec1922bd21b47711143b7969
Nicholas Gould <nrgould197@gmail.com>
2020-09-19 14:36:32 -0400

    init, improvements, & google analytics & hotjar tags

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml3');
+var textWrapper = document.querySelector(".ml3");
 textWrapper.innerHTML = textWrapper.textContent.replace(
 	/\S/g,
 	"<span class='letter'>$&</span>"
@@ -8,16 +8,16 @@ textWrapper.innerHTML = textWrapper.textContent.replace(
 anime
 	.timeline({ loop: false })
 	.add({
-		targets: '.ml3 .letter',
+		targets: ".ml3 .letter",
 		opacity: [0, 1],
-		easing: 'easeInOutQuad',
-		duration: 250,
+		easing: "easeInOutQuad",
+		duration: 150,
 		delay: (el, i) => 50 * (i + 1),
 	})
 	.add({
-		targets: '.ml3',
+		targets: ".ml3",
 		opacity: 1,
-		duration: 250,
-		easing: 'easeOutExpo',
+		duration: 150,
+		easing: "easeOutExpo",
 		delay: 0,
 	});
